0701f58bc7c66d6c8ed285e9161ee583af406aae
George Moon <george_moon@natural-solutions.eu>
2023-05-25 13:50:33 +0200

    Added image conversion

---
+++ export-images.config.js
@@ -0,0 +1,11 @@
+/**
+ * @type {import('next-export-optimize-images').Config}
+ */
+const config = {
+  convertFormat: [
+    ["png", "webp"],
+    ["jpg", "webp"],
+  ],
+};
+
+module.exports = config;
