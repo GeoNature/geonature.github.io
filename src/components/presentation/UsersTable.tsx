8bb1ff01dd3451412bdc3ba313e4e8acf850498d
George Moon <george_moon@natural-solutions.eu>
2023-06-29 18:29:55 +0200

    Added Download link to UsersTable

--- src/components/presentation/UsersTable.tsx
+++ src/components/presentation/UsersTable.tsx
@@ -1,25 +1,40 @@
+import { PropsWithChildren } from "react";
 import User from "@/interfaces/User";
+
+const DownloadLink = ({ data, children }: PropsWithChildren<{ data: any }>) => (
+  <a
+    href={`data:text/json;charset=utf-8,${encodeURIComponent(
+      JSON.stringify(data)
+    )}`}
+    download="data.json"
+  >
+    {children}
+  </a>
+);
 
 const UsersTable = ({ users }: { users: User[] }) => (
   <>
     {!users && <span>Il n'y a pas d'utilisateurs.</span>}
     {users && (
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Nom</th>
-            <th>Type</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user, index) => (
-            <tr key={index}>
-              <td>{user.name}</td>
-              <td>{user.type}</td>
+      <>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Nom</th>
+              <th>Type</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {users.map((user, index) => (
+              <tr key={index}>
+                <td>{user.name}</td>
+                <td>{user.type}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <DownloadLink data={users}>Télécharger</DownloadLink>
+      </>
     )}
   </>
 );
